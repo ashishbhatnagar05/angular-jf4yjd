7385c7c4b5e957763e7d5c575f9bfccc3349bc29
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2020-04-06 12:37:54 +0530

    added recipe summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RecipeListComponent } from '../recipe-list/recipe-list.component';
+import { RecipeSummaryComponent } from '../recipe-summary/recipe-summary.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,RecipeListComponent ],
+  declarations: [ AppComponent, HelloComponent,RecipeListComponent,RecipeSummaryComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
